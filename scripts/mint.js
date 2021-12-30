e250516d2025d665d7b0b12df3defdc6ae0a803b
Hazel <xp0729@gmail.com>
2021-12-30 11:53:18 +0800

    init

---
+++ scripts/mint.js
@@ -0,0 +1,10 @@
+async function main() {
+  const NFT = await hre.ethers.getContractFactory("CnM");
+  const { CONTRACT_ADDRESS } = process.env;
+  const contract = NFT.attach(CONTRACT_ADDRESS);
+  await contract.mint();
+}
+main().then(() => process.exit(0)).catch(error => {
+  console.error(error);
+  process.exit(1);
+});
